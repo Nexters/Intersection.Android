e92ca0fccf3bdb40467d060b147b3d78e49f05cc
Bob <bob@dailyhotel.co.kr>
2015-01-15 03:00:33 +0900

    + 시작 페이지 전환 구현 + 뷰 페이저를 통한 앱 소개 페이지 테스팅

--- app/src/main/java/com/nexters/intersection/intersectionapp/activity/MainActivity.java
+++ app/src/main/java/com/nexters/intersection/intersectionapp/activity/MainActivity.java
@@ -1,10 +1,11 @@
-package com.nexters.intersection.intersectionapp;
+package com.nexters.intersection.intersectionapp.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.nexters.intersection.intersectionapp.R;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/nexters/intersection/intersectionapp/activity/StartActivity.java
@@ -0,0 +1,33 @@
+package com.nexters.intersection.intersectionapp.activity;
+
+import com.nexters.intersection.intersectionapp.adapter.*;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.nexters.intersection.intersectionapp.R;
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.PageIndicator;
+
+public class StartActivity extends ActionBarActivity {
+    TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        mIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+    }
+}

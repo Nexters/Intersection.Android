cb55177b9e2fc476d589ed11769f483edd6c5095
Bob <bob@dailyhotel.co.kr>
2015-01-17 15:09:19 +0900

    오류 수정

--- app/src/main/java/com/nexters/intersection/intersectionapp/activity/StartActivity.java
+++ app/src/main/java/com/nexters/intersection/intersectionapp/activity/StartActivity.java
@@ -19,7 +19,6 @@ public class StartActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
 
         mAdapter = new TestFragmentAdapter(getSupportFragmentManager());

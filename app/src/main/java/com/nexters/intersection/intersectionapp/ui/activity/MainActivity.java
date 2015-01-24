0b26b69731b7e81a800e0921bea8c68373de082d
Bob <bob@dailyhotel.co.kr>
2015-01-24 15:30:27 +0900

    테스트 액티비티 추가

--- app/src/main/java/com/nexters/intersection/intersectionapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/nexters/intersection/intersectionapp/ui/activity/MainActivity.java
@@ -67,9 +67,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getSupportActionBar().setCustomView(R.layout.menu_main);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
